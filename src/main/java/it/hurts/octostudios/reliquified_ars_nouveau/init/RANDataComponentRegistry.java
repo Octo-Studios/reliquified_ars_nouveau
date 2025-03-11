21293d6f2bf16081c3b781e96cdbb209325926f1
iuak2 <iuak228@gmail.com>
2025-03-11 22:08:37 +0300

    Added a new item, but no visual effects, because im too lazy

--- src/main/java/it/hurts/octostudios/reliquified_ars_nouveau/init/RANDataComponentRegistry.java
+++ src/main/java/it/hurts/octostudios/reliquified_ars_nouveau/init/RANDataComponentRegistry.java
@@ -1,9 +1,9 @@
 package it.hurts.octostudios.reliquified_ars_nouveau.init;
 
-import com.hollingsworth.arsnouveau.api.spell.SpellCaster;
 import com.mojang.serialization.Codec;
 import it.hurts.octostudios.reliquified_ars_nouveau.ReliquifiedArsNouveau;
 import it.hurts.octostudios.reliquified_ars_nouveau.items.hands.MulticastedComponent;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.neoforged.bus.api.IEventBus;
@@ -11,6 +11,7 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.List;
+import java.util.UUID;
 
 public class RANDataComponentRegistry {
     public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENTS = DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, ReliquifiedArsNouveau.MODID);
@@ -18,6 +19,12 @@ public class RANDataComponentRegistry {
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<List<MulticastedComponent>>> MULTICASTED = DATA_COMPONENTS.register("multicasted",
             () -> DataComponentType.<List<MulticastedComponent>>builder()
                     .persistent(Codec.list(MulticastedComponent.CODEC))
+                    .build()
+    );
+
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<List<UUID>>> WOLVES = DATA_COMPONENTS.register("wolves",
+            () -> DataComponentType.<List<UUID>>builder()
+                    .persistent(UUIDUtil.CODEC.listOf())
                     .build()
     );
 
