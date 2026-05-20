125e1e3a723d8bc7050026e0670f8ac49535c8bc
SSKirillSS <SSTroveSS@Gmail.com>
2026-05-20 13:53:35 +0300

    Fixed docs URL

--- src/main/java/it/hurts/shatterbyte/reliquified_ars_nouveau/items/base/RANRelicItem.java
+++ src/main/java/it/hurts/shatterbyte/reliquified_ars_nouveau/items/base/RANRelicItem.java
@@ -2,8 +2,12 @@ package it.hurts.shatterbyte.reliquified_ars_nouveau.items.base;
 
 import it.hurts.shatterbyte.reliquified_ars_nouveau.ReliquifiedArsNouveau;
 import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class RANRelicItem extends RelicItem {
     public RANRelicItem(Item.Properties properties) {
@@ -19,5 +23,10 @@ public abstract class RANRelicItem extends RelicItem {
     @Override
     public String getConfigRoute() {
         return ReliquifiedArsNouveau.MODID;
+    }
+
+    @Override
+    public @Nullable String getURI(LivingEntity entity, ItemStack stack) {
+        return "https://shatterbyte.com/docs/mods/reliquified-ars-nouveau/relics/" + BuiltInRegistries.ITEM.getKey(stack.getItem()).getPath() + "/";
     }
 }
--- src/main/java/it/hurts/shatterbyte/reliquified_ars_nouveau/items/base/RANWearableRelicItem.java
+++ src/main/java/it/hurts/shatterbyte/reliquified_ars_nouveau/items/base/RANWearableRelicItem.java
@@ -2,10 +2,19 @@ package it.hurts.shatterbyte.reliquified_ars_nouveau.items.base;
 
 import it.hurts.shatterbyte.reliquified_ars_nouveau.ReliquifiedArsNouveau;
 import it.hurts.sskirillss.relics.items.relics.base.WearableRelicItem;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class RANWearableRelicItem extends WearableRelicItem {
     @Override
     public String getConfigRoute() {
         return ReliquifiedArsNouveau.MODID;
+    }
+
+    @Override
+    public @Nullable String getURI(LivingEntity entity, ItemStack stack) {
+        return "https://shatterbyte.com/docs/mods/reliquified-ars-nouveau/relics/" + BuiltInRegistries.ITEM.getKey(stack.getItem()).getPath() + "/";
     }
 }
